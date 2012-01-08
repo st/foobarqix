d6f64363855f8820fa5d8bfcda2477eeea0674ee
Stephane Tavera <stephane@inagua.ch>
2012-01-08 20:04:22 +0100

    documentation

--- src/foobarqix/core.clj
+++ src/foobarqix/core.clj
@@ -3,10 +3,15 @@
 ;; ## Foobarqix in Clojure
 
 ;; _[http://www.code-story.net/](http://www.code-story.net/) presents a little challenge called foobarqix. It's the first phase to select a pair of programmers that will be part of the team led by David and Jean-Laurent at devoxx (april 2012)._
+
 ;; The following code is not a submission.
-;; Instead a clojure solution (from an apprentice) written with simplicity in mind. I hope it may teach a little to newbies.
+;; Instead a clojure solution (from an apprentice) written with simplicity in mind. I hope it may teach a little to newbies.(Sources : [https://github.com/st/foobarqix](https://github.com/st/foobarqix))
+
 ;; Feedbacks very welcome
 ;; (@stephane_tavera on twitter)
+
+;; Marginalia is a fantastic tool (you're just reading its output). Code and documentation are "one and the same".
+;; Thank you @fogus ! [http://fogus.me/fun/marginalia/](http://fogus.me/fun/marginalia/)
 
 ;; # Rules
 ;; They are declared only once here. The data structure used is a map.
@@ -16,12 +21,13 @@
 
 ;; # to-ints
 ;; Convenience function returning the sequence of digits.
-;; Let's explain this function from inside to outside.We build a string with x (we are expecting x to be a number) thanks to (str x). We then take the sequence of characters with seq. And we substract 48 to the int value for each element.
-;; (The magic number 48 corresponds to the code of '0'.)
+;; Let's explain this function from inside to outside.We build a string with x (we are expecting x to be a number) thanks to (str x). We then take the sequence of characters with seq. And we substract 48 to the int value for each element.(48 is the code of '0'.)
 
 (defn to-ints 
 	[x]
     (map #(- (int %) 48) (seq (str x))))
+
+;; What better documentation than unit tests 	[here](https://github.com/st/foobarqix/blob/master/test/foobarqix/test/core.clj) ?
 
 ;; # has
 ;; This function applies the rule about presence of a digit.
@@ -30,24 +36,27 @@
 
 ;; We take advantage of the fact that a map `m` is also a function where (= value (m key)) and (nil? (m not-a-key)) for each key, value of `m`.
 ;; For example, (rules 2) returns nil, as (rules 5) returns Bar.
-;; `to-ints` retrieves the list of digits `x`. For each digit we apply the `rules` map (remember it's also a function) thanks to `mapcat`. 
-;; From `(doc mapcat)` : Returns the result of applying concat to the result of applying map to f and colls.  Thus function f should return a collection.
+;; `to-ints` retrieves the list of digits `x`. For each digit we apply and concate the `rules` map (remember it's also a function) thanks to `mapcat`. 
 
 (defn has [[x accu]]
     [x (apply str accu (mapcat rules (to-ints x)))])
 
-;; In case (very likely) where previous documentation was unclear, have a look at unit tests !
+;; In case (very likely) where previous documentation was unclear, have a look at unit tests [here](https://github.com/st/foobarqix/blob/master/test/foobarqix/test/core.clj) !
 
 ;; # div
 ;; This function applies the rules about the divisibility.
 ;; The functions corresponding to each number (3, 5 and 7) are actually produced by `div`.
+
 ;; A function that returns another function ? Welcome to the Clojure world. When a function outputs not a value but another function, we talk about a `higher-order` function.
+
 ;; The function returned is itself straightforward as it appends to `accu` only if the predicate (= 0 (rem x n)) is satisfied.
 (defn div 
 	[[n s]]
     (fn [[x accu]]
         [x (str accu (when (= 0 (rem x n)) s))]))        
         
+;; Unclear ? OK,you now know the way to at unit tests : [here](https://github.com/st/foobarqix/blob/master/test/foobarqix/test/core.clj)
+
 ;; # all
 ;; The functions `has` and `div` ones (one div function per rule entry) are just assembled in `all`. 
 ;; So `all` will compute the foobarqix rules for a given number.
